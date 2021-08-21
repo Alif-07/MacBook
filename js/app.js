0178e8c6817a49dd17e9c6ad756e1b1353b2bdfb
Alif-07 <mokaddis.alif@gmail.com>
2021-08-21 20:53:24 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -17,37 +17,49 @@ const promoCodeInput = document.getElementById('promo-code-input');
 btn8gb.addEventListener('click', function () {
 	extraMemory.innerText = '0';
 	totalPrice.innerText = parseInt(totalPrice.innerText) - parseInt(180);
+	footerTotal.innerText = parseInt(footerTotal.innerText) - parseInt(180);
 });
 btn16gb.addEventListener('click', function () {
 	extraMemory.innerText = '180';
 	totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt('180');
+	footerTotal.innerText = parseInt(footerTotal.innerText) + parseInt('180');
+	btn16gb.disabled = true;
 });
 btn256gb.addEventListener('click', function () {
 	extraStorage.innerText = '0';
 	totalPrice.innerText = parseInt(totalPrice.innerText) - parseInt(100);
+	footerTotal.innerText = parseInt(footerTotal.innerText) - parseInt(100);
 });
 btn512gb.addEventListener('click', function () {
 	extraStorage.innerText = '100';
 	totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt('100');
+	footerTotal.innerText = parseInt(footerTotal.innerText) + parseInt('100');
+	btn512gb.disabled = true;
 });
 btn1tb.addEventListener('click', function () {
 	extraStorage.innerText = '180';
 	totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt('180');
+	footerTotal.innerText = parseInt(footerTotal.innerText) + parseInt('180');
+	btn1tb.disabled = true;
 });
 deliveryFree.addEventListener('click', function () {
 	chargeFee.innerText = '0';
 	totalPrice.innerText = parseInt(totalPrice.innerText) - parseInt('20');
+	footerTotal.innerText = parseInt(footerTotal.innerText) - parseInt('20');
 });
 deliveryCharge.addEventListener('click', function () {
 	chargeFee.innerText = '20';
 	totalPrice.innerText = parseInt(totalPrice.innerText) + parseInt('20');
+	footerTotal.innerText = parseInt(footerTotal.innerText) + parseInt('20');
+
+	deliveryCharge.disabled = true;
 });
 promoCode.addEventListener('click', function () {
 	const promoCodeInputTxt = promoCodeInput.value;
 	if (promoCodeInputTxt == 'stevekaku') {
 		footerTotal.innerText =
 			parseFloat(totalPrice.innerText) - parseFloat(totalPrice.innerText) * 0.2;
-		console.log(totalPrice.innerText);
+		promoCode.disabled = true;
 	} else {
 		alert('Invalid Promo Code');
 	}
